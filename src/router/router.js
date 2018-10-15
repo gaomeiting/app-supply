f8f21258882ecdad615d58580a1cfeea23f82907
潘芸燕 <402015534@qq.com>
2018-10-15 09:16:02 +0800

    工作台首页及详情页完成

--- src/router/router.js
+++ src/router/router.js
@@ -39,6 +39,12 @@ export default new Router({
               component: () => import("@/views/order/fast.vue")
             },
             {
+              path: '/order/fastDetail',
+              name: 'order_fast_detail',
+              meta: {breadcrumbName: '快捷订单详情'},
+              component: () => import("@/views/order/fastDetail.vue")
+            },
+            {
               path: '/order/project',
               name: 'order_project',
               meta: {breadcrumbName: '项目订单'},
